b8b021ff387151bc78b0e769a70234a42b66b236
tony.yoo <l22491360@gmail.com>
2022-04-05 13:56:12 +0900

    start test for this module

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget https://035105f7-ae32-47b6-a25b-87af7924c7ea.filesusr.com/archives/3091be_233bace50f3f48fba40547a89443a96e.zip?dn=booking_dataset.zip -O booking_dataset.zip
+mkdir -p download
+unzip booking_dataset.zip -d download
+rm -rf booking_dataset.zip
